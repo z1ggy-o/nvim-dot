c60b169ab6648de12e76f2f380f5af8512ca8797
Guangyu Zhu <gy.zhu29@outlook.com>
2024-03-18 22:52:20 +0800

    [Add] plugin mini.indentscope.nvim instead of using indent_blank.nvim's default function
    But why? performance issue?

--- lua/plugins/indent_line.lua
+++ lua/plugins/indent_line.lua
@@ -1,10 +1,28 @@
 -- Show vertical line for each level of indent
 return {
-	{ -- Add indentation guides even on blank lines
-		"lukas-reineke/indent-blankline.nvim",
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help ibl`
-		main = "ibl",
-		opts = {},
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	opts = {
+		indent = {
+			char = "│",
+			tab_char = "│",
+		},
+		scope = { enabled = false },
+		exclude = {
+			filetypes = {
+				"help",
+				"alpha",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+				"lazyterm",
+			},
+		},
 	},
+	main = "ibl",
 }
