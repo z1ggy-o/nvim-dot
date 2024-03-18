44d9e3571bfaa973d73d7e05f2f43faf659b73b3
Guangyu Zhu <gy.zhu29@outlook.com>
2024-03-18 21:51:41 +0800

    [Plugin] revise gitsigns.nvim
    Copy & paste from LazyVim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,47 @@
+return {
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+	-- Highlights other instances under the cursor
+	{
+		"RRethy/vim-illuminate",
+		-- Learn from here https://github.com/LazyVim/LazyVim/discussions/1583
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		opts = {
+			delay = 200,
+			large_file_cutoff = 2000,
+			large_file_overrides = {
+				providers = { "lsp" },
+			},
+		},
+		config = function(_, opts)
+			require("illuminate").configure(opts)
 
-  -- Highlight todo, notes, etc in comments
-return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+			local function map(key, dir, buffer)
+				vim.keymap.set("n", key, function()
+					require("illuminate")["goto_" .. dir .. "_reference"](false)
+				end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+			end
+
+			map("]]", "next")
+			map("[[", "prev")
+
+			-- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					local buffer = vim.api.nvim_get_current_buf()
+					map("]]", "next", buffer)
+					map("[[", "prev", buffer)
+				end,
+			})
+		end,
+		keys = {
+			{ "]]", desc = "Next Reference" },
+			{ "[[", desc = "Prev Reference" },
+		},
+	},
+}
