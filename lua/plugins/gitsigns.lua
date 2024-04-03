10c96cadd9f6f0048911a9dd8c5496a1da198ec3
Guangyu Zhu <guangyu.zhu@innogritcorp.com>
2024-04-03 13:26:38 +0800

    [Revise] mini.surround key mappings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -23,6 +23,17 @@ return {
 			changedelete = { text = "▎" },
 			untracked = { text = "▎" },
 		},
+		-- show line blame by default
+		current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+			delay = 1000,
+			ignore_whitespace = false,
+			virt_text_priority = 100,
+		},
+		current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+
 		on_attach = function(buffer)
 			local gs = package.loaded.gitsigns
 			local function map(mode, l, r, desc)
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -14,7 +14,17 @@ return { -- Collection of various small independent plugins/modules
 		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 		-- - sd'   - [S]urround [D]elete [']quotes
 		-- - sr)'  - [S]urround [R]eplace [)] [']
-		require("mini.surround").setup()
+		require("mini.surround").setup({
+				mappings = {
+					add = 'gsa', -- Add surrounding in Normal and Visual modes
+					delete = 'gsd', -- Delete surrounding
+					find = 'gsf', -- Find surrounding (to the right)
+					find_left = 'gsF', -- Find surrounding (to the left)
+					highlight = 'gsh', -- Highlight surrounding
+					replace = 'gsr', -- Replace surrounding
+					update_n_lines = 'gsn', -- Update `n_lines`
+				}
+			})
 
 		-- Simple and easy statusline.
 		--  You could remove this setup call if you don't like it,
@@ -54,7 +64,6 @@ return { -- Collection of various small independent plugins/modules
 			end,
 			desc = "Delete Buffer",
 		},
-    -- stylua: ignore
-    { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+		{ "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
 	},
 }
