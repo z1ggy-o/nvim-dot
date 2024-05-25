71f7fe5cb3464e8c215db5641c0658ebca8067cc
Guangyu Zhu <gy.zhu29@outlook.com>
2024-05-25 15:24:04 +0800

    revise completion config

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -2,6 +2,22 @@ return { -- Autocompletion
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
+		-- Adds other completion capabilities.
+		--  nvim-cmp does not ship with all sources by default. They are split
+		--  into multiple repos for maintenance purposes.
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+
+		-- If you want to add a bunch of pre-configured snippets,
+		--    you can use this plugin to help you. It even has snippets
+		--    for various frameworks/libraries/etc. but you will have to
+		--    set up the ones that are useful for you.
+		-- 'rafamadriz/friendly-snippets',
+
+		-- Add vscode like pictogram to completion entries. (UI)
+		"onsails/lspkind.nvim",
+
 		-- Snippet Engine & its associated nvim-cmp source
 		{
 			"L3MON4D3/LuaSnip",
@@ -25,52 +41,64 @@ return { -- Autocompletion
 			end)(),
 		},
 		"saadparwaiz1/cmp_luasnip",
-
-		-- Adds other completion capabilities.
-		--  nvim-cmp does not ship with all sources by default. They are split
-		--  into multiple repos for maintenance purposes.
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-
-		-- If you want to add a bunch of pre-configured snippets,
-		--    you can use this plugin to help you. It even has snippets
-		--    for various frameworks/libraries/etc. but you will have to
-		--    set up the ones that are useful for you.
-		-- 'rafamadriz/friendly-snippets',
 	},
 	config = function()
+		-- require "custom.snippet" -- load personal snippets, do not have any yet.
+
+		local lspkind = require("lspkind")
+		lspkind.init({})
+
 		-- See `:help cmp`
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
-		luasnip.config.setup({})
+		luasnip.config.setup({
+			history = false,
+			updateevents = "TextChanged, TextChangedI",
+		})
 
 		cmp.setup({
+			-- TODO: what is the following line means? read :h complete option
+			completion = { completeopt = "menu,menuone,noinsert" },
+
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+				{ name = "buffer" },
+				-- { name = "luasnip" }, -- TJ does not have this, maybe cus we already have the snippet section
+			},
+			-- Enable luasnip to handle snippet expansion for nvim-cmp
 			snippet = {
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
 				end,
 			},
-			completion = { completeopt = "menu,menuone,noinsert" },
 
 			-- For an understanding of why these mappings were
 			-- chosen, you will need to read `:help ins-completion`
 			--
 			-- No, but seriously. Please read `:help ins-completion`, it is really good!
 			mapping = cmp.mapping.preset.insert({
-				-- Select the [n]ext item
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				-- Select the [p]revious item
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-				-- Scroll doc buffer
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-
 				-- Accept ([y]es) the completion.
 				--  This will auto-import if your LSP supports it.
 				--  This will expand snippets if the LSP sent a snippet.
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				-- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				-- ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+				["<C-y>"] = cmp.mapping(
+					cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true,
+					}),
+					{ "i", "c" }
+				),
+
+				-- Select the [n]ext item
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Inser }),
+				-- Select the [p]revious item
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Inser }),
+
+				-- Scroll doc buffer
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
 
 				-- Manually trigger a completion from nvim-cmp.
 				--  Generally you don't need this, because nvim-cmp will display
@@ -88,23 +116,17 @@ return { -- Autocompletion
 				--
 				-- <c-l> will move you to the right of each of the expansion locations.
 				-- <c-h> is similar, except moving you backwards.
-				["<C-l>"] = cmp.mapping(function()
+				["<C-j>"] = cmp.mapping(function()
 					if luasnip.expand_or_locally_jumpable() then
 						luasnip.expand_or_jump()
 					end
 				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
+				["<C-k>"] = cmp.mapping(function()
 					if luasnip.locally_jumpable(-1) then
 						luasnip.jump(-1)
 					end
 				end, { "i", "s" }),
 			}),
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
-			},
 		})
 	end,
 }
