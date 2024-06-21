ea96ad9c230e3159cd70085e302f207793f6ad68
Guangyu Zhu <guangyu.zhu@innogritcorp.com>
2024-06-21 18:35:18 +0800

    [CODEDING] add treesitter textobjects to move around functions

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,4 +1,39 @@
 return { -- Highlight, edit, and navigate code
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		event = "VeryLazy",
+		enabled = true,
+		config = function()
+			-- If treesitter is already loaded, we need to run config again for textobjects
+			-- if LazyVim.is_loaded("nvim-treesitter") then
+			-- 	local opts = LazyVim.opts("nvim-treesitter")
+			-- 	require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
+			-- end
+
+			-- When in diff mode, we want to use the default
+			-- vim text objects c & C instead of the treesitter ones.
+			local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+			local configs = require("nvim-treesitter.configs")
+			for name, fn in pairs(move) do
+				if name:find("goto") == 1 then
+					move[name] = function(q, ...)
+						if vim.wo.diff then
+							local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+							for key, query in pairs(config or {}) do
+								if q == query and key:find("[%]%[][cC]") then
+									vim.cmd("normal! " .. key)
+									return
+								end
+							end
+						end
+						return fn(q, ...)
+					end
+				end
+			end
+		end,
+	},
+
+	{
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
@@ -7,6 +42,24 @@ return { -- Highlight, edit, and navigate code
 		auto_install = true,
 		highlight = { enable = true },
 		indent = { enable = true },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			},
+		},
+		textobjects = {
+			move = {
+				enable = true,
+				goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+				goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+				goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+				goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+			},
+		},
 	},
 	config = function(_, opts)
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -21,4 +74,5 @@ return { -- Highlight, edit, and navigate code
 		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
 		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	end,
+	},
 }
