ee79e538301373f87f263f402056c4239c32854e
Guangyu <gy.zhu29@outlook.com>
2025-01-05 22:42:06 +0800

    [UI] config markview hybrid_modes
    When enable hybrid modes, the line that cursor on shows the raw data
    instead of the rendered one.
    For contents like links, we want this, otherwise it is hard to modify
    the link content itself.

--- lua/custom/plugins/markview.lua
+++ lua/custom/plugins/markview.lua
@@ -1,10 +1,16 @@
 return {
     "OXY2DEV/markview.nvim",
+    enabled = true,
     dependencies = {
         "nvim-tree/nvim-web-devicons", -- Used by the code bloxks
     },
 
-    config = function ()
-        require("markview").setup();
-    end
+    opts = {
+        hybrid_modes = { "n" },
+    },
+
+    -- If we use `opts` then we should not use setup() or we need to pass it to the setup func
+    -- config = function ()
+    --     require("markview").setup();
+    -- end
 }
