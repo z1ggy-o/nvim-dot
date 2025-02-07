939fe437cbdd550c96e4979dacfbb3f1fe76d52d
Guangyu Zhu <guangyu.zhu@innogritcorp.com>
2025-02-07 17:53:27 +0800

    (revise) markview setting update [UI]
    API update

--- lua/custom/plugins/markview.lua
+++ lua/custom/plugins/markview.lua
@@ -6,7 +6,9 @@ return {
     },
 
     opts = {
-        hybrid_modes = { "n" },
+        preview = {
+            hybrid_modes = { "n" },
+        },
     },
 
     -- If we use `opts` then we should not use setup() or we need to pass it to the setup func
