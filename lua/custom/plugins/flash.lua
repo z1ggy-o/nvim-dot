8bb210841aa2c7daa87c25b4699ba21201f9eef6
Guangyu <gy.zhu29@outlook.com>
2025-12-29 00:47:36 +0800

    [package] added: flash-zh.nvim to help jump to chinese characters with flypy

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -1,15 +1,25 @@
 -- Search by lables
 return {
-	"folke/flash.nvim",
-	event = "VeryLazy",
-	---@type Flash.Config
-	opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  {
+    "rainzm/flash-zh.nvim",
+    event = "VeryLazy",
+    dependencies = "folke/flash.nvim",
+    keys = {
+      { "s", mode = {"n", "x", "o"}, function() require("flash-zh").jump({ chinese_only = false }) end, desc = "Flash between Chinese" }
+    }
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  }
 }
