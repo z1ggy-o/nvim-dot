41f0a5e3e264160764dbb1727f6c806d8f52b1b3
Guangyu <gy.zhu29@outlook.com>
2026-01-11 12:21:52 +0800

    [new] add snacks.nvim and img-clip.nvim for doc image support [MOD:plugins][markdown]
    - snacks.nvim's image function helps us to view images in neovim (terminal emulator should support image view)
    - img-clip help copy/paste image from clipboard or file system to a specific directory (default is ./attachments from vim pwd)

---
+++ lua/custom/plugins/img-clip.lua
@@ -0,0 +1,47 @@
+return {
+	"HakonHarnes/img-clip.nvim",
+	event = "VeryLazy",
+	ft = {"markdown", 'typst', 'tex'},
+	opts = {
+		-- add options here
+		-- or leave it empty to use the default settings
+		default = {
+			dir_path = "./attachments",
+			use_absolute_path = false,
+			copy_images = true,
+			prompt_for_file_name = false,
+			file_name = "%y%m%d-%H%M%S",
+			extension = "avif",
+			process_cmd = "magick convert - -quality 75 avif:-",
+		},
+		filetypes = {
+			markdown = {
+				template = "![image$CURSOR]($FILE_PATH)",
+			},
+			tex = {
+				dir_path = "./figs",
+				extension = "png",
+				process_cmd = "",
+				template = [[
+		\begin{figure}[h]
+			\centering
+			\includegraphics[width=0.8\textwidth]{$FILE_PATH}
+		\end{figure}
+				]], ---@type string | fun(context: table): string
+			},
+			typst = {
+				dir_path = "./figs",
+				extension = "png",
+				process_cmd = "magick convert - -density 300 png:-",
+				formats = { "jpeg", "jpg", "png", "pdf", "svg" }, ---@type table
+				template = [[
+					#align(center)[#image("$FILE_PATH", height: 80%)]
+					]],
+			},
+		},
+	},
+	keys = {
+		-- suggested keymap
+		{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+	},
+}
